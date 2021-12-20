bcd910e8a6e2075a866da5cb315f35a22f84174a
Kaioru <keith@kaioru.co>
2021-12-20 14:32:44 +0800

    Add Config.h definitions for easy configuration

--- src/Blackwings/Config.cpp
+++ src/Blackwings/Config.cpp
@@ -3,22 +3,33 @@
 
 #include "Config.h"
 
-#define DEFAULT_CONFIG_FILE "./Blackwings.ini"
+#define CONFIG_LOAD_FROM_FILE   true
+#define CONFIG_FILE_PATH        "./Blackwings.ini"
+
+#define CONFIG_CONNECTION_HOST  "127.0.0.1"
+#define CONFIG_CONNECTION_PORT  8484
+#define CONFIG_GAME_TITLE       "Blackwings"
 
 ConfigConnection Config::Connection;
 ConfigGame Config::Game;
 
 void Config::Initialize() {
+#if CONFIG_LOAD_FROM_FILE
     char* sHost = new char[255];
 
-    GetPrivateProfileString("Connection", "Host", "127.0.0.1", sHost, 255, DEFAULT_CONFIG_FILE);
+    GetPrivateProfileString("Connection", "Host", CONFIG_CONNECTION_HOST, sHost, 255, CONFIG_FILE_PATH);
 
     Connection.Host = sHost;
-    Connection.Port = GetPrivateProfileInt("Connection", "Port", 8484, DEFAULT_CONFIG_FILE);
+    Connection.Port = GetPrivateProfileInt("Connection", "Port", CONFIG_CONNECTION_PORT, CONFIG_FILE_PATH);
 
     char* sTitle = new char[255];
 
-    GetPrivateProfileString("Game", "Title", "Blackwings", sTitle, 255, DEFAULT_CONFIG_FILE);
+    GetPrivateProfileString("Game", "Title", CONFIG_GAME_TITLE, sTitle, 255, CONFIG_FILE_PATH);
 
     Game.Title = sTitle;
+#else
+    Connection.Host = CONFIG_CONNECTION_HOST;
+    Connection.Port = CONFIG_CONNECTION_PORT;
+    Game.Title = CONFIG_GAME_TITLE;
+#endif
 }
