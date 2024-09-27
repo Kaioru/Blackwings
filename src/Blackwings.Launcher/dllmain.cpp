c471d2bd7f95128c54b71211a390be0b60c72ea7
Kaioru <keith@kaioru.co>
2024-09-27 13:28:50 +0800

    Add injector project

---
+++ src/Blackwings.Launcher/dllmain.cpp
@@ -0,0 +1,34 @@
+// dllmain.cpp : Defines the entry point for the DLL application.
+#include "pch.h"
+
+#define LIB_INJECT_NAME "Blackwings.dll"
+
+INT WINAPI WinMain(
+    HINSTANCE hInstance, 
+    HINSTANCE hPrevInstance,
+    PSTR pCmdLine, 
+    int nCmdShow
+) {
+    STARTUPINFOA si;
+    PROCESS_INFORMATION pi;
+    ZeroMemory(&si, sizeof(si));
+    ZeroMemory(&pi, sizeof(pi));
+
+    if (!DetourCreateProcessWithDllExA(
+        "MapleStory.exe", 
+        NULL, NULL, NULL, FALSE, CREATE_SUSPENDED, NULL, NULL, &si, &pi, LIB_INJECT_NAME, NULL
+    )) {
+        return 1;
+    }
+
+    ResumeThread(pi.hThread);
+    WaitForSingleObject(pi.hProcess, INFINITE);
+
+    DWORD dwExitCode;
+
+    if (!GetExitCodeProcess(pi.hProcess, &dwExitCode)) {
+        return 1;
+    }
+
+    return dwExitCode;
+}
